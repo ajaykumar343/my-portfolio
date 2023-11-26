ad7d12e2bbc7cd9d4ad743e146169e8893b4a3c1
ajaykumar343 <ajaykumar.jtr@gmail.com>
2023-11-26 16:20:33 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -81,7 +81,7 @@ function NavBar() {
 
            <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/ajaykumar343/my-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
